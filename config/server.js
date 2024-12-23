fb004f9d8685905613f23683fee136f35f10aeea
ishugoel21 <goelishu868@gmail.com>
2024-12-23 15:17:15 +0530

    updated commit

--- config/server.js
+++ config/server.js
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', process.env.PORT || 1337),
-  url: env('PUBLIC_URL', 'https://pizza47.azurewebsites.net'),
+  host: env('HOST', '0.0.0.0'), // Ensures Strapi listens on all available interfaces
+  port: env.int('PORT', process.env.PORT || 1337), // Dynamically sets the port
+  url: env('PUBLIC_URL', 'https://pizza47-fdb3h37h8-ishugoel21s-projects.vercel.app'), // Your public URL
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  proxy: true, // Ensures Azure proxies are handled correctly
+  proxy: true, // Optional: usually used for reverse proxies like Azure
 });
