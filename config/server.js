75b158b198f6cc472b1e41545f81e2e92498ed37
ishugoel21 <goelishu868@gmail.com>
2024-12-23 16:02:13 +0530

    new commit

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'), // Ensures Strapi listens on all available interfaces
   port: env.int('PORT', process.env.PORT || 1337), // Dynamically sets the port
-  url: env('PUBLIC_URL', 'https://pizza47-fdb3h37h8-ishugoel21s-projects.vercel.app'), // Your public URL
+  url: env('PUBLIC_URL', 'https://pizza67.vercel.app/'), // Your public URL
   app: {
     keys: env.array('APP_KEYS'),
   },
